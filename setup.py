d277c9ae81cb78550f5f9495b48d5fe350a4eda4
klaudiusz223 <klaudiusz223@users.noreply.github.com>
2020-04-19 17:29:15 +0200

    disabling pycomfoconnect automatic install (wrong version)

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ setuptools.setup(
     install_requires=[
         'Homie4>=0.3.2',
         'pyyaml',
-        'pycomfoconnect',
+        # 'pycomfoconnect',
     ],
     dependency_links=[
-        'git+https://github.com/klaudiusz223/comfoconnect.git@patch-3#egg=pycomfoconnect-0.3.0'
+        # 'git+https://github.com/klaudiusz223/comfoconnect.git@patch-3#egg=pycomfoconnect-0.3.0'
     ],
     python_requires='>=3.6',
 )
