42fa5d797320635c4e2c49e9787e008bf476e0ac
klaudiusz223 <klaudiusz223@users.noreply.github.com>
2021-09-01 12:42:54 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="comfoairq-homie", 
-    version="0.6.1",
+    version="0.6.2",
     author="Klaudiusz",
     author_email="klaudiusz223@users.noreply.github.com",
     description="Homie4 for Zehnder ComfoAirQ",
